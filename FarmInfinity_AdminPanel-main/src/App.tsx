7cfbf8d539cc04f64e270e25a85e146c9928bb7e
waahacabs <waahacabs@gmail.com>
2025-05-14 15:23:38 +0000

    fixed login error

--- FarmInfinity_AdminPanel-main/src/App.tsx
+++ FarmInfinity_AdminPanel-main/src/App.tsx
@@ -1,8 +1,7 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Sidebar from "./components/Sidebar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useLocation, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Farmers from "./components/Farmers";
 import Agent from "./components/Agent";
 import BankAgent from "./components/BankAgent";
@@ -13,6 +12,7 @@ import LoginAdmin from "./components/LoginAdmin";
 import Staff from "./components/Staff";
 import FarmerApplication from "./components/FarmerApplication";
 import HomePage from "./components/HomePage";
+import { useLocation, Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ element }: any) => {
   const isAuthenticated = localStorage.getItem("token"); // Check for token in local storage
@@ -31,52 +31,50 @@ function App() {
   ].includes(location.pathname);
 
   return (
-    <Router>
-      <div className="flex h-screen">
-        {!hideSidebar && <Sidebar />}
-        <div className={`flex-1 overflow-auto ${!hideSidebar ? "ml-48" : ""}`}>
-          {" "}
-          {/* Adjusted ml to ml-48 for left margin */}
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login-admin" element={<LoginAdmin />} />
-            <Route path="/login-agent" element={<LoginAgent />} />
-            <Route
-              path="/dashboard"
-              element={<PrivateRoute element={<Dashboard />} />}
-            />
-            <Route
-              path="/fpo"
-              element={<PrivateRoute element={<FPO />} />}
-            />
-            <Route
-              path="/staff"
-              element={<PrivateRoute element={<Staff />} />}
-            />
-            <Route
-              path="/farmers"
-              element={<PrivateRoute element={<Farmers />} />}
-            />
-            <Route
-              path="/agent"
-              element={<PrivateRoute element={<Agent />} />}
-            />
-            <Route
-              path="/bank-agent"
-              element={<PrivateRoute element={<BankAgent />} />}
-            />
-            <Route
-              path="/farmers_details/farmerId/:farmerId/applicationId/:applicationId"
-              element={<PrivateRoute element={<FarmerDetails />} />}
-            />
-            <Route
-              path="/farmers_applications/:id"
-              element={<PrivateRoute element={<FarmerApplication />} />}
-            />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+ <div className="flex h-screen">
+ {!hideSidebar && <Sidebar />}
+ <div className={`flex-1 overflow-auto ${!hideSidebar ? "ml-48" : ""}`}>
+ {" "}
+ {/* Adjusted ml to ml-48 for left margin */}
+ <Routes>
+ <Route path="/" element={<HomePage />} />
+ <Route path="/login-admin" element={<LoginAdmin />} />
+ <Route path="/login-agent" element={<LoginAgent />} />
+ <Route
+ path="/dashboard"
+ element={<PrivateRoute element={<Dashboard />} />}
+ />
+ <Route
+ path="/fpo"
+ element={<PrivateRoute element={<FPO />} />}
+ />
+ <Route
+ path="/staff"
+ element={<PrivateRoute element={<Staff />} />}
+ />
+ <Route
+ path="/farmers"
+ element={<PrivateRoute element={<Farmers />} />}
+ />
+ <Route
+ path="/agent"
+ element={<PrivateRoute element={<Agent />} />}
+ />
+ <Route
+ path="/bank-agent"
+ element={<PrivateRoute element={<BankAgent />} />}
+ />
+ <Route
+ path="/farmers_details/farmerId/:farmerId/applicationId/:applicationId"
+ element={<PrivateRoute element={<FarmerDetails />} />}
+ />
+ <Route
+ path="/farmers_applications/:id"
+ element={<PrivateRoute element={<FarmerApplication />} />}
+ />
+ </Routes>
+ </div>
+    </div>
   );
 }
 
--- FarmInfinity_AdminPanel-main/src/main.tsx
+++ FarmInfinity_AdminPanel-main/src/main.tsx
@@ -1,18 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-// import keycloak from './keycloak';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import keycloak from './keycloak';
+import { BrowserRouter } from 'react-router-dom';
 
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <ReactKeycloakProvider
-//     authClient={keycloak}
-//     initOptions={{
-//       onLoad: 'login-required',
-//       checkLoginIframe: false,
-//     }}
-//   >
-//     <App />
-//   </ReactKeycloakProvider>
-// );
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ReactKeycloakProvider
+    authClient={keycloak}
+    initOptions={{
+      onLoad: 'login-required',
+      checkLoginIframe: false,
+    }}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ReactKeycloakProvider>
+);
